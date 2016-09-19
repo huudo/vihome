7f5f47f4cfc95474e2346af2fc629cb8cbf3cb88
Trong Huu <huudt.3012@gmail.com>
2016-09-19 16:27:50 +0700

    ads

--- resources/views/page/project.blade.php
+++ resources/views/page/project.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.default')
 @section('content')
+<div class="row">
+    <div class="col-md-12 top_head">
 
+    </div>
+</div>
+
+<div class="row container_detail">
+    <div class="col-md-12">
+        
+    </div>
+</div>
 @stop
